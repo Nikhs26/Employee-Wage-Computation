8e2553efdc959cc0a9c6db4674aff813f3d0ba80
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-08 15:32:51 +0530

    Refactor the Code to write a function to get work hours

--- empWage.sh
+++ empWage.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -x
+#! /bin/bash
 
 isPartTime=1
 isFullTime=2
@@ -8,24 +8,26 @@ noWorkingDays=20
 totalEmpHr=0;
 totalWorkingDays=0;
 
+function getWorkingHours(){
+case $1 in
+        $isPartTime)
+                empHrs=4
+                ;;
+        $isFullTime)
+                empHrs=8
+                ;;
+        *)
+                empHrs=0
+                ;;
+esac
+echo $empHrs
+}
+
 while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $noWorkingDays ]]
 do
 ((totalWorkingDays++))
-empCheck=$((RANDOM%3))
-
-case $empCheck in
-	$isPartTime)
-		empHrs=4
-		;;
-	$isFullTime)
-		empHrs=8
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-
-totalEmpHr=$(($totalEmpHr+$empHrs))
+workHrs=$( getWorkingHours $((RANDOM%3)) )
+totalEmpHr=$(($totalEmpHr+$workHrs))
 done
 totalSalary=$(($totalEmpHr*$empRatePerHr))
 echo $totalSalary
