88136720add71fd77ea7d417e1d5f9df70a8f146
Nikhil Gupta <nikhilgahoi1731997@gmail.com>
2022-01-08 14:53:19 +0530

    Solving using Switch Case Statement

--- empWage.sh
+++ empWage.sh
@@ -5,15 +5,17 @@ isFullTime=2
 empRatePerHr=20
 empCheck=$((RANDOM%3))
 
-if [ $isPartTime -eq $empCheck ];
-then
-	empHrs=4
-elif [ $isFullTime -eq $empCheck ];
-then
-	empHrs=8
-else
-	empHrs=0
-fi
+case $empCheck in
+	$isPartTime)
+		empHrs=4
+		;;
+	$isFullTime)
+		empHrs=8
+		;;
+	*)
+		empHrs=0
+		;;
+esac
 
 salary=$(($empHrs*$empRatePerHr))
 echo $salary
